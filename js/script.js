16b6b056274fedc7f26f33aef94b9ab4088fcf89
Omkar Wagh <167704070+Omkar-Wagh@users.noreply.github.com>
2025-03-15 03:32:23 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -73,3 +73,35 @@ document.getElementById("contact-form").addEventListener("submit", async functio
         alert("❌ Error sending message. Please try again.");
     }
 });
+
+// bmi
+function calculate() {
+    var bmi;
+    var result = document.getElementById("result");
+
+    var weight = parseInt(document.getElementById("weight").value);
+    document.getElementById("weight-val").textContent = weight + " kg";
+
+    var height = parseInt(document.getElementById("height").value);
+    document.getElementById("height-val").textContent = height + " cm";
+
+    bmi = (weight / Math.pow((height / 100), 2)).toFixed(1);
+    result.textContent = bmi;
+
+    var category = "";
+    if (bmi < 18.5) {
+        category = "Underweight 😒";
+        result.style.color = "#ffc44d";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        category = "Normal Weight 😍";
+        result.style.color = "#0be881";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        category = "Overweight 😮";
+        result.style.color = "#ff884d";
+    } else {
+        category = "Obese 😱";
+        result.style.color = "#ff5e57";
+    }
+
+    document.getElementById("category").textContent = category;
+}
